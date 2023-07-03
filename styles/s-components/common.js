404c48726eb245afa625a446fd2413b95526ee97
Mmaryyy <marynrvly@gmail.com>
2023-07-03 17:53:02 +0900

    [style] global style, common components

---
+++ styles/s-components/common.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import "./../s-global/variable.css";
+
+// layout
+export const OuterWrapper = styled.main`
+  width: 100%;
+  overflow: auto;
+  display: flex;
+  padding: 50px 15px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContentWrapper = styled.section`
+  width: 90%;
+  background-color: var(--white-bg);
+  padding: ${(props) => props.padding || "0"};
+  margin: 10px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+// contents
+export const DivisionLine = styled.hr`
+  width: 30%;
+  height: 1.5px;
+  background-color: var(--gold-300);
+  border: 0;
+  margin-bottom: 30px;
+`;
+
+export const Content = styled.p`
+  font-size: ${(props) => props.fz || `var(--fz-base)`};
+  line-height: 1.8;
+  @media (max-width: 720px) {
+    font-size: var(--fz-base);
+  }
+`;
---
+++ styles/s-global/GlobalStyle.js
@@ -0,0 +1,72 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0; padding: 0; border: none; background: none;
+    font-size: 100%; font-weight: var(--fw-base); font-family: var(--font-nanum);
+    -webkit-tap-highlight-color: transparent;
+  }
+  ol,ul {
+    list-style: none;
+  } 
+  strong {
+    font-weight: var(--fw-black);
+  }
+  img {
+    vertical-align: top;
+    outline: none;
+  }
+  hr {
+    display: block;
+    width: 100%;
+    border-style: solid;
+  }
+  input,select,textarea,button {
+    appearance: none; -webkit-appearance: none;
+    outline: none; background: none;
+    border: none; border-radius: 0;
+    margin: 0; padding: 0; line-height: 1;
+  }
+  input:-webkit-autofill, 
+  input:-webkit-autofill:hover, 
+  input:-webkit-autofill:focus, 
+  input:-webkit-autofill:active {
+    background: transparent; background-clip: text; -webkit-background-clip: text;
+    -webkit-text-fill-color: var(--black-400);
+  }
+  textarea {
+    resize: none;
+  }
+  ::placeholder {
+    font: inherit; 
+  }
+  a {
+    text-decoration: none; color: inherit;
+  }
+  a:visited {
+    background: none; color: inherit;
+  }
+  a:active {
+    background: none; color: inherit;
+  }
+
+  body {
+    background-image: url("/assets/bg.png");
+    background-size: cover;
+    overflow-x: hidden;
+    /* line-height: 1; */
+    font-size: var(--fz-base);
+    color: var(--black-400);
+    font-family: var(--font-nanum),
+    -apple-system, 
+    BlinkMacSystemFont, 
+    system-ui, 
+    Roboto, 
+    'Helvetica Neue', 
+    'Segoe UI', 
+    'Apple SD Gothic Neo', 
+    'Noto Sans KR', 
+    'Malgun Gothic', sans-serif;
+  }
+`;
