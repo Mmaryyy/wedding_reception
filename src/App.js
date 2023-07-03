9858fe59e56ec2b3e3197c525e436c43531eaa2e
Mmaryyy <marynrvly@gmail.com>
2023-07-03 19:06:38 +0900

    [feat] UI 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/s-global/variable.css'
 import { GlobalStyle } from './styles/s-global/GlobalStyle'
+import Invite from './components/Invite'
+import Intro from './components/Intro'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <GlobalStyle />
+        <Invite />
+        <Intro />
       </BrowserRouter>
     </>
   )
