ff7c31de2eacf0ec25796ac259c4fd451eaaf967
Mmaryyy <marynrvly@gmail.com>
2023-07-05 22:21:07 +0900

    [refactor] 불필요한 코드 삭제

--- src/styles/s-components/carousel.js
+++ src/styles/s-components/carousel.js
@@ -31,7 +31,6 @@ export const ControllContainer = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  /* background-color: #fff; */
   display: flex;
   flex-direction: column;
   justify-content: center;
