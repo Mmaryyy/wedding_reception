157d218d2323cbe1f808d1c31b23943f0cf07eb3
Mmaryyy <marynrvly@gmail.com>
2023-07-17 00:10:15 +0900

    [refactor] Contact 컴포넌트 수정

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,35 +4,38 @@ import { ContactWrapper, DropdownWrapper } from '../styles/s-components/contact'
 import Dropdown from './Dropdown'
 
 const Contact = () => {
-  const daddy = {
-    name: '이 춘 홍',
-    phoneNumber: '01086253548',
-    bank: '농협',
-    account: '601145-52-134819',
-  }
-  const mommy = {
-    name: '김 순 덕',
-    phoneNumber: '01071743548',
-    bank: '국민',
-    account: '766101-01-616129',
-  }
+  const parents = [
+    {
+      parent: 'daddy',
+      name: '이 춘 홍',
+      phoneNumber: '01086253548',
+      bank: '농협',
+      account: '601145-52-134819',
+    },
+    {
+      parent: 'mommy',
+      name: '김 순 덕',
+      phoneNumber: '01071743548',
+      bank: '국민',
+      account: '766101-01-616129',
+    },
+  ]
 
   return (
     <>
-      <ContactWrapper className="daddy">
-        <Content className="strong">{daddy.name}</Content>
-        <DropdownWrapper className="dropdown_wrapper">
-          <Dropdown isForCalling={true} info={daddy} />
-          <Dropdown isForCalling={false} info={daddy} />
-        </DropdownWrapper>
-      </ContactWrapper>
-      <ContactWrapper className="mommy">
-        <Content className="strong">{mommy.name}</Content>
-        <DropdownWrapper className="dropdown_wrapper">
-          <Dropdown isForCalling={true} info={mommy} />
-          <Dropdown isForCalling={false} info={mommy} />
-        </DropdownWrapper>
-      </ContactWrapper>
+      {parents.map((el, idx) => {
+        return (
+          <ContactWrapper className={`${el.parent}`} key={idx}>
+            <Content className="strong" color="var(--gold-400)">
+              {el.name}
+            </Content>
+            <DropdownWrapper className="dropdown_wrapper">
+              <Dropdown isForCalling={true} info={el} />
+              <Dropdown isForCalling={false} info={el} />
+            </DropdownWrapper>
+          </ContactWrapper>
+        )
+      })}
     </>
   )
 }
