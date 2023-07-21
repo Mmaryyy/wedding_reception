9c152290e1539706262edf018ef3e2472be38823
Mmaryyy <marynrvly@gmail.com>
2023-07-21 22:57:59 +0900

    [feat] modal 구현

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import isPropValid from '@emotion/is-prop-valid'
-import { StyleSheetManager } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
 import './styles/s-global/variable.css'
-import { GlobalStyle } from './styles/s-global/GlobalStyle'
-import { Provider } from 'react-redux'
-import { store } from './store'
+
 import {
-  InnerWrapper,
   InnerBox,
+  InnerWrapper,
   OuterWrapper,
 } from './styles/s-components/common'
-import Invite from './components/Invite'
-import Intro from './components/Intro'
+
+import { BrowserRouter } from 'react-router-dom'
 import Gallery from './components/Gallery'
+import { GlobalStyle } from './styles/s-global/GlobalStyle'
 import Greeting from './components/Greeting'
+import Intro from './components/Intro'
+import Invite from './components/Invite'
 import Modal from './components/common/Modal'
+import React from 'react'
+import { StyleSheetManager } from 'styled-components'
+import isPropValid from '@emotion/is-prop-valid'
+import { useSelector } from 'react-redux'
 
 function App() {
+  const { isOpen } = useSelector((state) => state.modal)
   return (
     <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <StyleSheetManager shouldForwardProp={isPropValid}>
-            <GlobalStyle />
-            <Modal />
-            <OuterWrapper>
-              <InnerWrapper>
-                <InnerBox>
-                  <Invite />
-                  <Intro />
-                  <Gallery />
-                  <Greeting />
-                </InnerBox>
-              </InnerWrapper>
-            </OuterWrapper>
-          </StyleSheetManager>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <StyleSheetManager shouldForwardProp={isPropValid}>
+          <GlobalStyle />
+          {isOpen && <Modal />}
+          <OuterWrapper>
+            <InnerWrapper>
+              <InnerBox>
+                <Invite />
+                <Intro />
+                <Gallery />
+                <Greeting />
+              </InnerBox>
+            </InnerWrapper>
+          </OuterWrapper>
+        </StyleSheetManager>
+      </BrowserRouter>
     </>
   )
 }
