f0890314a55f24724e2d86f1ac6dc0e890b39cec
Mmaryyy <marynrvly@gmail.com>
2023-07-21 16:27:07 +0900

    [refactor] Dropdown - drop menu 컴포넌트 분리

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Content } from '../styles/s-components/common'
 import { ContactWrapper, DropdownWrapper } from '../styles/s-components/contact'
-import Dropdown from './Dropdown'
+import Dropdown from './common/Dropdown'
 
 const Contact = () => {
-  const parents = [
-    {
-      parent: 'daddy',
-      name: '이 춘 홍',
-      phoneNumber: '01086253548',
-      bank: '농협',
-      account: '601145-52-134819',
-    },
-    {
-      parent: 'mommy',
-      name: '김 순 덕',
-      phoneNumber: '01071743548',
-      bank: '국민',
-      account: '766101-01-616129',
-    },
-  ]
-
+  const count = Object.keys(useSelector((state) => state.parents))
+  const parents = useSelector((state) => state.parents)
   return (
     <>
-      {parents.map((el, idx) => {
+      {count.map((el, idx) => {
         return (
-          <ContactWrapper className={`${el.parent}`} key={idx}>
+          <ContactWrapper className={`${el}`} key={idx}>
             <Content className="strong" color="var(--gold-400)">
-              {el.name}
+              {parents[el].name}
             </Content>
-            <DropdownWrapper className="dropdown_wrapper">
-              <Dropdown isForCalling={true} info={el} />
-              <Dropdown isForCalling={false} info={el} />
+            <DropdownWrapper>
+              <Dropdown isCall={true} parent={parents[el]} />
+              <Dropdown isCall={false} parent={parents[el]} />
             </DropdownWrapper>
           </ContactWrapper>
         )
