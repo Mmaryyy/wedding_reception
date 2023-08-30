8f6c20634bc231b4da3fa467a4dcf6551078e0cd
Mmaryyy <marynrvly@gmail.com>
2023-08-30 18:25:32 +0900

    [fix] defaultValue update시 value 값 업데이트 되도록 수정

--- src/utils/useInput.js
+++ src/utils/useInput.js
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue)
   const onChange = (e) => {
     setValue(e.target.value)
   }
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
   return { value, onChange }
 }
 
