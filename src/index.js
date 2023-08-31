741dfe2336cb37c62903e16c6926382e537020a2
Mmaryyy <marynrvly@gmail.com>
2023-08-31 22:55:43 +0900

    [fix] modal - update error handling comment 수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import App from './App'
+import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App'
 import { store } from './store/index'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
